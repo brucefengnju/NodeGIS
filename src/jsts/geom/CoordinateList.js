3c88f3661daa0555c2f02ff2ff95989c365f317d
brucechen <brucechenofnju@gmail.com>
2013-05-21 15:56:28 +0800

    update lisence

--- src/jsts/geom/CoordinateList.js
+++ src/jsts/geom/CoordinateList.js
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011 by The Authors.
+/* Copyright (c) 2013 by The Authors.
  * Published under the LGPL 2.1 license.
  * See /license-notice.txt for the full text of the license notice.
  * See /license.txt for the full text of the license.
